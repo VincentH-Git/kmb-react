8374b72f65b4c177f89499a816b852e4518e2697
Vincent Hung <vincenthung3131@gmail.com>
2022-08-03 03:19:44 +0800

    added header

--- src/components/BusListing.tsx
+++ src/components/BusListing.tsx
@@ -3,10 +3,28 @@ import { useNavigate, useParams } from "react-router-dom";
 import '../css/BusListing.css';
 
 export default function BusListing() {
+
+  const fetchData = async()=>{
+    let res = await fetch(
+      "https://data.weather.gov.hk/weatherAPI/opendata/weather.php?dataType=flw&lang=tc"
+    );
+    let data = await res.json();
+    console.log(data)
+  }
+
+  useEffect(()=>{
+
+
+  })
   let navigate = useNavigate();
   let { id } = useParams()
   return (
     <div className="container">
+      <div className='header'>
+        <div className="text">
+          KMB 1933
+        </div>
+      </div>
       <div className='row'>
         <div className='busNumberContainer'>
           <div className='busNumberText'>
